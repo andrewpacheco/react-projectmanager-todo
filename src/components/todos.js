dbefcaec3d2af9162c671d30db22abbee2c04cc6
Andrew Pacheco <35854617+andrewpacheco@users.noreply.github.com>
2018-05-25 00:04:24 -0700

    Add files via upload

---
+++ src/components/todos.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TodoItem from './todoitem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+    render() {
+        let todoItems;
+        if(this.props.todos){
+            todoItems = this.props.todos.map(todo => {
+                //console.log(project);
+                return (
+                    <TodoItem key={todo.title} todo={todo} />
+                )
+            })
+        }
+    console.log(this.props);
+    return (
+      <div className="Todos">
+        <h3>Todo List</h3>
+        {todoItems}
+      </div>
+    );
+  }
+}
+
+export default Todos;
